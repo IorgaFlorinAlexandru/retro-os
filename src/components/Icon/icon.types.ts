a825421492e84bbd44281bee4a58e713eabc924e
Iorga Florin <iorgaflorinalexandru@gmail.com>
2025-07-29 23:24:48 +0300

    Added more win95 icons

--- src/components/Icon/icon.types.ts
+++ src/components/Icon/icon.types.ts
@@ -1,5 +1,13 @@
 export enum Icons {
     FOLDER = 'folder.ico',
     WINDOWS = 'windows.ico',
-    TEXT_DOCUMENT = 'text_document.ico'
+    TEXT_DOCUMENT = 'text_document.ico',
+    MY_COMPUTER = 'my_computer.ico',
+    PROGRAM = 'program.ico',
+    SHORTCUT = 'shortcut.ico',
+    TURNOFF_COMPUTER = 'turnoff_computer.ico',
+    EMPTY_RECYCLE_BIN = 'empty_recycle_bin.ico',
+    PAINT = 'paint.ico',
+    DRIVE = 'drive.ico',
+    OPENED_FOLDER = 'opened_folder.ico',
 }
