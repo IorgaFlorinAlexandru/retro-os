d172910f7d5b3d326d24197d0284597fec306e4f
Alex <iorgaflorinalexandru@gmail.com>
2025-10-05 22:11:47 +0300

    Moved button to start menu component

--- src/os/explorer/components/TaskBar.tsx
+++ src/os/explorer/components/TaskBar.tsx
@@ -1,58 +1,17 @@
 import styles from './TaskBar.module.css';
-import Button from "../../../components/Button/Button.tsx";
-import Icon from "../../../components/Icon/Icon.tsx";
-import { Icons } from "../../../components/Icon/icon.types.ts";
 import StartMenu from "../../start-menu/components/StartMenu.tsx";
-import {RefObject, useEffect, useRef, useState} from "react";
 import {useContextMenu} from "../../../hooks/useContextMenu.ts";
 import {ContextAction} from "../../../types/context-menu.types.ts";
 import TaskbarContextMenu from "../../../context/components/TaskbarContextMenu.tsx";
 
 export default function TaskBar() {
-    const [open, setOpen] = useState(false);
-    const btnRef: RefObject<HTMLButtonElement | null> = useRef(null)
-    const menuRef: RefObject<HTMLDivElement | null> = useRef(null);
-
     const ref = useContextMenu<ContextAction>(TaskbarContextMenu, (value) => {
         console.log(value);
     });
 
-    useEffect(() => {
-        const btn = btnRef.current;
-        const menu = menuRef.current;
-        if(!menu || !btn) return;
-
-        const handler = (event: MouseEvent) => {
-            const target = event.target as HTMLElement;
-            if(menu.contains(target) || btn.contains(target)) return;
-            setOpen(false);
-        }
-
-        if(open) {
-            document.body.addEventListener("click", handler, false);
-        }
-
-        return () => {
-            document.body.removeEventListener("click", handler, false);
-        }
-    }, [open]);
-
-    const handleStartAction = () => {
-        setOpen(!open);
-    }
-
-
     return <div ref={ref} className={styles.container}>
         <div className={styles.win95TaskBar}>
-            <div className={styles.win95StartContainer}>
-                <StartMenu ref={menuRef} open={open}></StartMenu>
-                <Button ref={btnRef}
-                        className={styles.startButton}
-                        onClick={handleStartAction}>
-                    <Icon src={Icons.WINDOWS} size='sm'></Icon>
-                    <span>Start</span>
-                </Button>
-            </div>
+            <StartMenu></StartMenu>
             <div className={`win95-inset ${styles.systemTray}`}>
                 <span>12:35 AM</span>
             </div>
--- src/os/start-menu/components/StartMenu.tsx
+++ src/os/start-menu/components/StartMenu.tsx
@@ -1,18 +1,48 @@
 import styles from './StartMenu.module.css';
-import {RefObject} from "react";
+import {RefObject, useLayoutEffect, useRef, useState} from "react";
+import Button from "../../../components/Button/Button.tsx";
+import Icon from "../../../components/Icon/Icon.tsx";
+import {Icons} from "../../../components/Icon/icon.types.ts";
 
-export default function StartMenu({open, ref}: StartMenuProps) {
+export default function StartMenu() {
+    const [open, setOpen] = useState(false);
+    const menuRef: RefObject<HTMLDivElement | null> = useRef(null);
 
-    if(open) {
-        return <div ref={ref} className={`win95-control ${styles.win95StartMenu}`}>
-            <div className={styles.win95Logo}>
-                <h1>Windows<span>95</span></h1>
-            </div>
-        </div>
+    useLayoutEffect(() => {
+        const menu = menuRef.current;
+        if(!menu) return;
+
+        const handler = (event: MouseEvent) => {
+            const target = event.target as HTMLElement;
+            if(!menu.contains(target)) {
+                setOpen(false);
+            }
+        }
+
+        if(open) {
+            document.body.addEventListener("mousedown", handler, false);
+        }
+
+        return () => {
+            document.body.removeEventListener("mousedown", handler, false);
+        }
+    }, [open]);
+
+    const handleStartAction = () => {
+        setOpen(!open);
     }
-}
 
-interface StartMenuProps {
-    open: boolean;
-    ref?: RefObject<HTMLDivElement | null>;
+    return  <div ref={menuRef} className={styles.win95StartContainer}>
+        <Button onClick={handleStartAction}>
+            <Icon src={Icons.WINDOWS} size='sm'></Icon>
+            <span>Start</span>
+        </Button>
+        {open && (
+            <div className={`win95-control ${styles.win95StartMenu}`}>
+                <div className={styles.win95Logo}>
+                    <h1>Windows<span>95</span></h1>
+                </div>
+            </div>
+        )}
+    </div>
 }
