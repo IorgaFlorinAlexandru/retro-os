fd6d6c450a4cdcf12dd8874430a96342034d1083
Alex <iorgaflorinalexandru@gmail.com>
2026-09-11 19:25:40 +0300

    Implemented the tab component

--- src/components/Tabs/Tabs.tsx
+++ src/components/Tabs/Tabs.tsx
@@ -1,24 +1,49 @@
 import {Children, isValidElement, ReactElement, ReactNode, useMemo, useState} from "react";
-import Tab, {TabProps} from "./Tab.tsx";
 import styles from './Tabs.module.css';
 
-export default function Tabs({ children }: { children: ReactNode }) {
+interface TabProps {
+    id: string;
+    label: string;
+    children: ReactNode;
+}
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+export function Tab(_: TabProps) {
+    return null;
+}
+
+export function Tabs({ children, defaultTabId }: { children: ReactNode, defaultTabId?: string }) {
     const tabs = useMemo(() => {
        return Children.toArray(children)
            .filter((child): child is ReactElement<TabProps> =>
                isValidElement(child) && child.type === Tab);
-    },[children])
+    },[children]);
 
-    const [activeTab, setActiveTab] = useState<ReactElement<TabProps> | null>(null)
+    const initialTabId = getInitialActiveTabId(tabs.map(t => t.props.id), defaultTabId);
+    const [activeTabId, setActiveTabId] = useState<string | null>(initialTabId);
 
     return <div className={styles.osTabs}>
         <ul className={styles.osTabList}>
             {tabs.map((tab, index) => (
-                <li className={`win95-control ${styles.osTab}`} key={index}>{tab.props.label}</li>
+                <li key={index}
+                    className={`win95-control ${styles.osTab} ${activeTabId === tab.props.id ? styles.activeTab : ''}`}
+                    onMouseDown={() => setActiveTabId(tab.props.id)}>{tab.props.label}</li>
             ))}
         </ul>
         <div className={`win95-control ${styles.osTabContent}`}>
-            {activeTab && (activeTab.props.children)}
+            {activeTabId && (tabs.find(t => t.props.id === activeTabId)?.props.children)}
         </div>
     </div>
+}
+
+function getInitialActiveTabId(tabIds: string[] ,defaultTabId?: string): string | null {
+    if(tabIds.length === 0) {
+        return null;
+    }
+
+    if (defaultTabId && tabIds.includes(defaultTabId)) {
+        return defaultTabId;
+    }
+
+    return tabIds[0];
 }
--- src/os/applications/TestComponent.tsx
+++ src/os/applications/TestComponent.tsx
@@ -1,8 +1,6 @@
 import {Window} from "../window";
 import styles from './TestComponent.module.css';
 import {Icons} from "../../components/Icon/icon.types.ts";
-import Tabs from "../../components/Tabs/Tabs.tsx";
-import Tab from "../../components/Tabs/Tab.tsx";
 
 export function TestComponent() {
     return <>
@@ -10,20 +8,7 @@ export function TestComponent() {
             <Window.TitleBar title={"My computer Test"} icon={Icons.MY_COMPUTER}></Window.TitleBar>
             <Window.MenuBar></Window.MenuBar>
             <div className={styles.testContent}>
-                <Tabs>
-                    <Tab value="1" label="General">
-                        hello
-                    </Tab>
-                    <Tab value="2" label="Device Manager">
-                        hello
-                    </Tab>
-                    <Tab value="3" label="Hardware Profiles">
-                        hello
-                    </Tab>
-                    <Tab value="4" label="Performance">
-                        hello
-                    </Tab>
-                </Tabs>
+                hello
             </div>
             <Window.StatusBar></Window.StatusBar>
         </Window.Root>
