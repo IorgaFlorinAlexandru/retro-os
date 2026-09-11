fd63bc7ab91e13f1a13893ead4ad6b62360d0846
IorgaFlorinAlexandru <72227691+IorgaFlorinAlexandru@users.noreply.github.com>
2026-09-11 19:27:41 +0300

    feature/RTOS-0007: Implement the tab component
    * Initial commit
    
    * Commiting changes
    
    * Implemented the tab component
    
    * Uncommented css styles

--- src/components/Tabs/Tabs.tsx
+++ src/components/Tabs/Tabs.tsx
@@ -1,4 +1,49 @@
-export default function Tabs() {
-    return <>
-    </>
+import {Children, isValidElement, ReactElement, ReactNode, useMemo, useState} from "react";
+import styles from './Tabs.module.css';
+
+interface TabProps {
+    id: string;
+    label: string;
+    children: ReactNode;
+}
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+export function Tab(_: TabProps) {
+    return null;
+}
+
+export function Tabs({ children, defaultTabId }: { children: ReactNode, defaultTabId?: string }) {
+    const tabs = useMemo(() => {
+       return Children.toArray(children)
+           .filter((child): child is ReactElement<TabProps> =>
+               isValidElement(child) && child.type === Tab);
+    },[children]);
+
+    const initialTabId = getInitialActiveTabId(tabs.map(t => t.props.id), defaultTabId);
+    const [activeTabId, setActiveTabId] = useState<string | null>(initialTabId);
+
+    return <div className={styles.osTabs}>
+        <ul className={styles.osTabList}>
+            {tabs.map((tab, index) => (
+                <li key={index}
+                    className={`win95-control ${styles.osTab} ${activeTabId === tab.props.id ? styles.activeTab : ''}`}
+                    onMouseDown={() => setActiveTabId(tab.props.id)}>{tab.props.label}</li>
+            ))}
+        </ul>
+        <div className={`win95-control ${styles.osTabContent}`}>
+            {activeTabId && (tabs.find(t => t.props.id === activeTabId)?.props.children)}
+        </div>
+    </div>
+}
+
+function getInitialActiveTabId(tabIds: string[] ,defaultTabId?: string): string | null {
+    if(tabIds.length === 0) {
+        return null;
+    }
+
+    if (defaultTabId && tabIds.includes(defaultTabId)) {
+        return defaultTabId;
+    }
+
+    return tabIds[0];
 }
