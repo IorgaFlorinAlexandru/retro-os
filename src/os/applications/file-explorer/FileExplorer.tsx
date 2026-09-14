bfbab7a6447198166f1a8fb17ecbaafeee4ee1dd
Alex <iorgaflorinalexandru@gmail.com>
2026-09-14 21:47:40 +0300

    Added base structure and styles for file explorer window status bar

--- src/os/applications/file-explorer/FileExplorer.tsx
+++ src/os/applications/file-explorer/FileExplorer.tsx
@@ -2,7 +2,7 @@ import {Window} from "../../window";
 import {Icons} from "../../../components/Icon/icon.types.ts";
 import styles from "./FileExplorer.module.css";
 import {useStorage} from "../../../contexts/StorageContext.tsx";
-import {useMemo} from "react";
+import {useMemo, useState} from "react";
 import FileManager from "../../file-manager/components/FileManager.tsx";
 
 export default function FileExplorer({ title, icon, filePath} : { title: string; icon: Icons, filePath: string }) {
@@ -17,6 +17,9 @@ export default function FileExplorer({ title, icon, filePath} : { title: string;
        return storage.files.filter(f => f.parentId === folder.id);
     },[storage, filePath]);
 
+    const [ selectionInfo, setSelectionInfo ] = useState<string>("1 object(s) selected");
+    const [ storageInfo, setStorageInfo ] = useState<string>("Free Space: 1.89GB, Capacity: 1.99GB");
+
     return <Window.Root>
         <Window.TitleBar title={title} icon={icon}></Window.TitleBar>
         <Window.MenuBar></Window.MenuBar>
@@ -24,7 +27,12 @@ export default function FileExplorer({ title, icon, filePath} : { title: string;
             <FileManager files={files}></FileManager>
         </div>
         <Window.StatusBar>
-
+            <div className={`win95-inset ${styles.statusPane} ${styles.selectionInfo} `}>
+                {selectionInfo}
+            </div>
+            <div className={`win95-inset ${styles.statusPane} ${styles.storageInfo}`}>
+                {storageInfo}
+            </div>
         </Window.StatusBar>
     </Window.Root>
 }
--- src/os/window/components/WindowStatusBar.tsx
+++ src/os/window/components/WindowStatusBar.tsx
@@ -1,13 +1,14 @@
 import styles from './WindowStatusBar.module.css';
 import {useWindowContext} from "../context/WindowContext.tsx";
+import {ReactNode} from "react";
 
-export function WindowStatusBar() {
+export function WindowStatusBar({ children }: {children?: ReactNode}) {
     const windowContext = useWindowContext();
 
     return <div className={styles.win95StatusBar}>
+        {children}
         <div className={styles.resizeHandle}
              onMouseDown={(e) => windowContext.resize(e,"SE")}>
-            <div></div>
         </div>
     </div>;
 }
